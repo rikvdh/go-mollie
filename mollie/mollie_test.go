6021701c8cc40549529414483329cd864be339d6
Rik van der Heijden <mail@rikvanderheijden.com>
2017-02-11 14:45:04 +0100

    Cover main api

---
+++ mollie/mollie_test.go
@@ -0,0 +1,19 @@
+package mollie
+
+import (
+	"testing"
+)
+
+func TestMollie(t *testing.T) {
+	m := Get("")
+
+	if m.Issuers() == nil {
+		t.Errorf("Issuers is nil")
+	}
+	if m.Methods() == nil {
+		t.Errorf("Issuers is nil")
+	}
+	if m.Payments() == nil {
+		t.Errorf("Issuers is nil")
+	}
+}
