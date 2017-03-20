a374cc39e70dbe8f9c19342561a4d195d7d69d73
theobouwman <theobouwman98@gmail.com>
2017-03-20 16:49:47 +0100

    customers

--- customers.go
+++ customers.go
@@ -1,9 +1,19 @@
 package mollie
 
-import "time"
+import (
+	"time"
+	"strconv"
+)
 
 type CustomerAPI struct {
 	c *core
+}
+
+type Links struct {
+	Previous string `json:"previous,omitempty"`
+	Next string `json:"next,omitempty"`
+	First string `json:"first,omitempty"`
+	Last string `json:"last,omitempty"`
 }
 
 type NewCustomerData struct {
@@ -11,6 +21,14 @@ type NewCustomerData struct {
 	Email string `json:"email"`
 	Locale string `json:"locale,omitempty"` // One of de_DE en_US es_ES fr_FR nl_BE fr_BE nl_NL
 	Metadata string `json:"metadata,omitempty"`
+}
+
+type ListCustomersResponse struct {
+	Totalcount uint `json:"totalcount"`
+	Offset uint `json:"offset"`
+	Count uint `json:"count"`
+	Data []Customer `json:"data"`
+	Links Links `json:"links,omitempty"`
 }
 
 type Customer struct {
@@ -50,3 +68,55 @@ func (api *CustomerAPI) Get(id string) (*Customer, error) {
 
 	return customer, nil
 }
+
+func (api *CustomerAPI) All(offset uint64, limit uint64) (*ListCustomersResponse, error) {
+	uri := "customers?offset="
+	uri += strconv.FormatUint(offset, 10)
+	uri += "&count="
+	uri += strconv.FormatUint(limit, 10)
+
+	response := &ListCustomersResponse{}
+	err := api.c.Get(uri, response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+func (api *CustomerAPI) NewPayment(id string, data PaymentData) (*PaymentReply, error) {
+	payment := &PaymentReply{}
+
+	err := api.c.Post("customers/" + id + "/payments", payment, data)
+	if err != nil {
+		return nil, err
+	}
+
+	return payment, nil
+}
+
+func (api *CustomerAPI) Payments(id string, offset uint64, limit uint64) (*paymentReplyWrapper, error) {
+	uri := "customers/" + id + "/payments?offset="
+	uri += strconv.FormatUint(offset, 10)
+	uri += "&count="
+	uri += strconv.FormatUint(limit, 10)
+
+	response := &paymentReplyWrapper{}
+	err := api.c.Get(uri, response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+
+
+
+
+
+
+
+
+
+
